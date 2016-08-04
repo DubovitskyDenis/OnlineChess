ae3b3d3515c345ac32eff034c8ae3bd53f32e9ab
Kirill Aleksandrov <xakepgame@gmail.com>
2016-08-04 17:46:45 +0700

    fix routing

--- config/routing.php
+++ config/routing.php
@@ -5,5 +5,5 @@ return [
     'POST game/cancel/<game_id>' => 'game/cancel',
     'POST game/surrender/<game_id>' => 'game/surrender',
     'GET game/<game_id>/move/<from_x>:<from_y>:<to_x>:<to_y>' => 'game/move',
-    'GET game/<game_id>/possible_moves/<from_x>:<from_y>' => 'game/possible_moves',
+    'GET game/<game_id>/possible_moves/<from_x>:<from_y>' => 'game/possible-moves',
 ];
